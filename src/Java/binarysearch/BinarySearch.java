449978ebd305d2d0e126b7a09ad1fe9a433f612d
Sanjay Babu <cksanjay97@gmail.com>
2022-05-09 09:53:33 +0530

    Adding binary search source code

---
+++ src/Java/binarysearch/BinarySearch.java
@@ -0,0 +1,18 @@
+package Java.binarysearch;
+
+public class BinarySearch {
+    public int search(int[] nums, int target) {
+        int left=0, right=nums.length-1;
+        while(left<=right){
+            int mid=(right+left)/2;
+            if(nums[mid]<target){
+                left=mid+1;
+            } else if (nums[mid]>target){
+                right=mid-1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
